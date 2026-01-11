bcd7d13f22dff400cb0c4503dd070222e21f1f73
Wanjirrru <melissawanjiru@yahoo.com>
2026-01-11 09:05:32 +0300

    Changed endpoint to try somwthing

--- client/src/components/ProductControl.js
+++ client/src/components/ProductControl.js
@@ -78,7 +78,7 @@ class ProductControl extends Component {
     }
     
     componentDidMount(){
-        axios.get('http://localhost:5000/api/products')
+        axios.get(`${API_URL}/api/products`)
             .then(res =>{
                 console.log(res)
                 this.setState({
